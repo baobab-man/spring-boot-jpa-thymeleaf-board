e6e3e85becbedefd7d6d6697dccf9ecae6fcd35d
romeo <ii.kim@fitpet.co.kr>
2021-11-30 21:50:29 +0900

    Initial commit

---
+++ src/test/java/Romeo/blackboard/BlackBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Romeo.blackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
